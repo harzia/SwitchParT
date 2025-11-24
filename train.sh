86ea1552ad313046065a4fe006fbacce74cca8c7
har-zia <harzia360@gmail.com>
2025-11-23 21:26:19 -0800

    Fix quick training script problem

--- train.sh
+++ train.sh
@@ -65,7 +65,7 @@ suffix=${COMMENT}
 NGPUS=${DDP_NGPUS:-1}
 
 if ((NGPUS > 1)); then
-    CMD="torchrun --standalone --nnodes=1 --nproc_per_node=$NGPUS $(which weaver) --backend nccl"
+    CMD="torchrun --standalone --nnodes=1 --nproc_per_node=$NGPUS -- $(which weaver) --backend nccl"
 else
     CMD="weaver"
 fi
